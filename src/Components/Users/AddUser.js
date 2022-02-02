f1d21a5ccea2fcba2d53883ffd79b80874389424
farzanemaleki <farzane.maleki.fm@gmail.com>
2022-02-02 13:01:26 +0330

    added error modal for form validation message

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -2,11 +2,13 @@ import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 import styles from './AddUser.module.css';
 
 const AddUser = props => {
     const [enteredName, setEnteredName] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const nameChangeHandler = (event) => {
         setEnteredName(event.target.value);
@@ -18,6 +20,20 @@ const AddUser = props => {
 
     const formSubmitHandler = event => {
         event.preventDefault();
+        if(enteredName.trim().length === 0 || enteredAge.trim().length === 0){
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name and age (non-empty values).',
+            });
+            return;
+        };
+        if( +enteredAge < 1 ){
+            setError({
+                title: 'Invalid age',
+                message: 'Please enter a valid age (> 0).',
+            });
+            return;
+        }
         const newUser = {
             id: Math.round(Math.random()*1000) ,
             name: enteredName,
@@ -26,9 +42,21 @@ const AddUser = props => {
         props.onAddUser(newUser);
         setEnteredName('');
         setEnteredAge('');
-    }
+    };
+
+    const errorHandler = () =>{
+        setError(null);
+    };
 
     return(
+      <>
+        {error && (
+            <ErrorModal
+                title={error.title}
+                message={error.message}
+                onConfirm={errorHandler}
+            />
+        )}
         <form onSubmit={formSubmitHandler}>
             <Card className={styles.form}>
                 <div className={styles['form-control']} >
@@ -50,6 +78,7 @@ const AddUser = props => {
                 <Button type="submit">Add User</Button>
             </Card>
         </form>
+      </>
     );
 }
 
